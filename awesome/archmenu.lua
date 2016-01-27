6e29121b1b098c35838d8bb9004d2a708f36f360
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2016-01-28 00:20:49 +0100

    regenerated xdg menu

--- awesome/archmenu.lua
+++ awesome/archmenu.lua
@@ -1,13 +1,18 @@
  local menu98edb85b00d9527ad5acebe451b3fae6 = {
      {"Android File Transfer (MTP)", "android-file-transfer", "/usr/share/icons/hicolor/128x128/apps/android-file-transfer.png" },
      {"Archive Manager", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
+     {"Ark", "ark -qwindowtitle Ark ", "/usr/share/icons/hicolor/16x16/apps/ark.png" },
      {"Desktop Search", "tracker-needle", "/usr/share/icons/gnome/16x16/actions/system-search.png" },
+     {"Files", "nautilus --new-window ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
+     {"GVim", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
+     {"Jupyter Qt console", "jupyter qtconsole", "///usr/lib/python3.5/site-packages/qtconsole/resources/icon/JupyterConsole.svg" },
+     {"KWrite", "kwrite ", "/usr/share/icons/hicolor/16x16/apps/kwrite.png" },
      {"Nemo", "nemo ", "/usr/share/icons/gnome/16x16/places/folder.png" },
+     {"Redshift", "redshift-gtk"},
      {"Screenshot", "gnome-screenshot --interactive", "/usr/share/icons/gnome/16x16/apps/applets-screenshooter.png" },
-     {"Vi IMproved", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
      {"Weather", "gapplication launch org.gnome.Weather.Application", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Weather.Application.png" },
-     {"ipython", "xterm -e ipython"},
-     {"ipython2", "xterm -e ipython2"},
+     {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"ipython2", "xterm -e ipython2", "/usr/share/pixmaps/ipython2.png" },
  }
 
  local menude7a22a0c94aa64ba2449e520aa20c99 = {
@@ -18,6 +23,7 @@
      {"Astromenace", "astromenace", "///usr/share/pixmaps/astromenace.png" },
      {"Chess", "gnome-chess", "/usr/share/icons/hicolor/16x16/apps/gnome-chess.png" },
      {"Dosbox", "dosbox", "/usr/share/pixmaps/dosbox.png" },
+     {"Mari0", "mari0"},
      {"SuperTux 2", "supertux2", "/usr/share/pixmaps/supertux.png" },
      {"Xboard", "xboard", "/usr/share/icons/hicolor/48x48/apps/xboard.png" },
  }
@@ -27,6 +33,7 @@
      {"GNU Image Manipulation Program", "gimp-2.8 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
      {"Image Viewer", "eog ", "/usr/share/icons/hicolor/16x16/apps/eog.png" },
      {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"Rapid Photo Downloader", "rapid-photo-downloader", "/usr/share/icons/hicolor/16x16/apps/rapid-photo-downloader.png" },
      {"gThumb", "gthumb ", "/usr/share/icons/hicolor/16x16/apps/gthumb.png" },
  }
@@ -41,15 +48,18 @@
      {"ELinks", "xterm -e /usr/bin/elinks ", "/usr/share/icons/gnome/16x16/mimetypes/html.png" },
      {"Empathy", "empathy", "/usr/share/icons/hicolor/16x16/apps/empathy.png" },
      {"Firefox", "firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
-     {"Liferea", "liferea", "/usr/share/icons/hicolor/16x16/apps/liferea.png" },
-     {"Mumble", "mumble"},
+     {"Mumble", "mumble "},
+     {"QupZilla", "qupzilla ", "/usr/share/icons/hicolor/16x16/apps/qupzilla.png" },
+     {"Remote Desktop Viewer", "vinagre ", "/usr/share/icons/gnome/16x16/apps/preferences-desktop-remote-desktop.png" },
      {"SSH AskPass", "/usr/bin/ssh-add"},
+     {"Skype", "skype ", "/usr/share/icons/hicolor/16x16/apps/skype.png" },
      {"Tor-Browser (en-US)", "/usr/bin/tor-browser-en", "///usr/share/pixmaps/tor-browser-en.png" },
      {"Transmission", "transmission-gtk ", "/usr/share/icons/hicolor/16x16/apps/transmission.png" },
      {"Web", "epiphany ", "/usr/share/icons/gnome/16x16/apps/web-browser.png" },
  }
 
  local menudf814135652a5a308fea15bff37ea284 = {
+     {"Calendar", "gnome-calendar", "/usr/share/icons/hicolor/16x16/apps/gnome-calendar.png" },
      {"Dictionary", "gnome-dictionary", "/usr/share/icons/gnome/16x16/apps/accessories-dictionary.png" },
      {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
      {"Evolution", "evolution ", "/usr/share/icons/hicolor/16x16/apps/evolution.png" },
@@ -60,6 +70,7 @@
      {"LibreOffice Impress", "libreoffice --impress ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-impress.png" },
      {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
      {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"WordNet", "wnb", "///usr/share/pixmaps/wordnet.png" },
      {"Zathura", "zathura "},
      {"Zotero", "/usr/bin/zotero ", "/usr/share/icons/hicolor/16x16/apps/zotero.png" },
@@ -67,6 +78,9 @@
 
  local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
      {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
+     {"Cuttlefish", "cuttlefish"},
+     {"Jupyter Qt console", "jupyter qtconsole", "///usr/lib/python3.5/site-packages/qtconsole/resources/icon/JupyterConsole.svg" },
+     {"Plasma Theme Explorer", "plasmathemeexplorer", "/usr/share/icons/gnome/16x16/apps/preferences-desktop-theme.png" },
      {"Qt Assistant", "/usr/lib/qt/bin/assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
      {"Qt Designer", "/usr/lib/qt/bin/designer", "/usr/share/icons/hicolor/128x128/apps/QtProject-designer.png" },
      {"Qt Linguist", "/usr/lib/qt/bin/linguist", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
@@ -75,8 +89,8 @@
      {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
      {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
      {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
-     {"ipython", "xterm -e ipython"},
-     {"ipython2", "xterm -e ipython2"},
+     {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"ipython2", "xterm -e ipython2", "/usr/share/pixmaps/ipython2.png" },
  }
 
  local menu52dd1c847264a75f400961bfb4d1c849 = {
@@ -84,13 +98,11 @@
      {"Cheese", "cheese", "/usr/share/icons/hicolor/16x16/apps/cheese.png" },
      {"Clementine", "clementine ", "/usr/share/icons/hicolor/64x64/apps/clementine.png" },
      {"FFADO Mixer", "ffado-mixer", "/usr/share/pixmaps/ffado.png" },
-     {"Lollypop", "lollypop ", "/usr/share/icons/hicolor/16x16/apps/lollypop.png" },
      {"Plasma Media Center", "plasmashell -ap org.kde.plasma.mediacenter"},
      {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/hicolor/16x16/apps/multimedia-volume-control.png" },
      {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
      {"Sound Recorder", "gnome-sound-recorder", "/usr/share/icons/hicolor/16x16/apps/gnome-sound-recorder.png" },
      {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
-     {"Vocal", "vocal", "/usr/share/icons/hicolor/22x22/apps/vocal.svg" },
      {"mpv Media Player", "mpv --profile=pseudo-gui -- ", "/usr/share/icons/hicolor/16x16/apps/mpv.png" },
  }
 
@@ -98,15 +110,20 @@
      {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
      {"Color Profile Viewer", "gcm-viewer", "/usr/share/icons/hicolor/16x16/apps/gnome-color-manager.png" },
      {"Configuration Editor", "gconf-editor", "/usr/share/icons/hicolor/16x16/apps/gconf-editor.png" },
+     {"Cool Retro Term", "cool-retro-term", "/usr/share/icons/hicolor/32x32/apps/cool-retro-term.png" },
      {"Dolphin", "dolphin ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
      {"GParted", "/usr/bin/gparted_polkit ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
      {"Hardware Locality lstopo", "lstopo"},
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
      {"KSysGuard", "ksysguard ", "/usr/share/icons/gnome/16x16/apps/utilities-system-monitor.png" },
+     {"Konsole", "konsole", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"Pacman Log Viewer", "plv", "/usr/share/icons/hicolor/16x16/apps/plv.png" },
      {"SSH AskPass", "/usr/bin/ssh-add"},
      {"Sakura", "sakura", "/usr/share/pixmaps/terminal-tango.svg" },
      {"Terminal", "gnome-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
      {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
      {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"gtk2fontsel", "gtk2fontsel"},
      {"urxvt", "urxvt", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
      {"urxvt (client)", "urxvtc", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
      {"urxvt (tabbed)", "urxvt-tabbed", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
