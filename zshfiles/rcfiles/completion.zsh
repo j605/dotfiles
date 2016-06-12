90ed455d5ab6483965e367e3f4648d797bb7dcd9
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2016-06-12 16:03:29 +0200

    added more filetype completions for mpv

--- zshfiles/rcfiles/completion.zsh
+++ zshfiles/rcfiles/completion.zsh
@@ -1,2 +1,6 @@
 # Suppress cwd from cd completion
 zstyle ':completion:*' ignore-parents parent pwd ..
+# Use LS_COLORS during tab completion
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# Add extra file-patterns for mpv
+zstyle ':completion:*:*:mpv:*' file-patterns '*.(#i)(aac|m4a)(-.) %p:globbed-files *(-/):directories' '*:all-files'
