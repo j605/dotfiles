02fe3852d12618ce25a3289f26dfe65cdf65d7b3
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2017-05-19 16:14:40 +0200

    zsh: add mpv completions for .wma

--- zshfiles/rcfiles/completion.zsh
+++ zshfiles/rcfiles/completion.zsh
@@ -3,4 +3,4 @@ zstyle ':completion:*' ignore-parents parent pwd ..
 # Use LS_COLORS during tab completion
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # Add extra file-patterns for mpv
-zstyle ':completion:*:*:mpv:*' file-patterns '*.(#i)(aac|m4a)(-.) %p:globbed-files *(-/):directories' '*:all-files'
+zstyle ':completion:*:*:mpv:*' file-patterns '*.(#i)(aac|m4a|wma)(-.) %p:globbed-files *(-/):directories' '*:all-files'
