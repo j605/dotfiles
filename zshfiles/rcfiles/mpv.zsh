cc08eff14d43f0bbe3f2728ea80b90596c16e15a
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2017-11-28 19:09:58 +0100

    zsh: mpv alias to play all music

--- zshfiles/rcfiles/mpv.zsh
+++ zshfiles/rcfiles/mpv.zsh
@@ -1,2 +1,5 @@
 alias mpi="mpv --profile=image"
 alias mpa="mpv --profile=dontshowimage"
+
+# play all music using mpv
+alias mpv-shuffle="mpv --profile=dontshowimage --gapless-audio --playlist <(shuf <(find ~/Music -regextype posix-extended -regex \".*/*\.(mp3|wav|flac|m4a)$\"))"
