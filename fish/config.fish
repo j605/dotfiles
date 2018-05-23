80c78f9890647ab3618cf43f28ab9086b7a55111
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2018-05-24 00:36:27 +0200

    fish: bump GL_VERSION for mpv and delete prompt
    This also includes the mpv image macro from zsh.

--- fish/config.fish
+++ fish/config.fish
@@ -5,31 +5,41 @@ set fish_greeting
 
 #{{{ Non autoload funtions
 function rmpv --wraps=mpv --description="mpv with radeon hwdec (X)"
-  env MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1 \
-    VDPAU_DRIVER=r600 LIBVA_DRIVER_NAME=vdpau \
+  env MESA_GL_VERSION_OVERRIDE=4.4 MESA_GLSL_VERSION_OVERRIDE=440 DRI_PRIME=1 \
+    VDPAU_DRIVER=r600 \
     mpv --profile=radeon_x11 $argv
 end
 
+function rmpv_vdpau --wraps=mpv --description="mpv with radeon hwdec (X)"
+  env MESA_GL_VERSION_OVERRIDE=4.4 MESA_GLSL_VERSION_OVERRIDE=440 DRI_PRIME=1 \
+    VDPAU_DRIVER=r600 \
+    mpv --profile=radeon_x11 --hwdec=vdpau $argv
+end
+
 function wrmpv --wraps=mpv --description="mpv with radeon hwdec (wayland)"
-  env MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1 \
-    VDPAU_DRIVER=r600 LIBVA_DRIVER_NAME=vdpau \
+  env MESA_GL_VERSION_OVERRIDE=4.4 MESA_GLSL_VERSION_OVERRIDE=440 DRI_PRIME=1 \
+    LIBVA_DRIVER_NAME=r600 \
     mpv --profile=radeon_wayland $argv
 end
 
 function wrmpv_dbg --wraps=mpv --description="mpv with radeon hwdec (wayland debug)"
-  env WAYLAND_DEBUG=client MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1 \
-    VDPAU_DRIVER=r600 LIBVA_DRIVER_NAME=vdpau \
+  env WAYLAND_DEBUG=client MESA_GL_VERSION_OVERRIDE=4.4 MESA_GLSL_VERSION_OVERRIDE=440 DRI_PRIME=1 \
+    LIBVA_DRIVER_NAME=r600 \
     mpv --profile=radeon_wayland $argv
 end
 
 function mpv-shuffle --description="shuffle and play all songs in a given folder or default to ~/Music"
   set -l music_folder "$HOME/Music"
   if count $argv > /dev/null
-    set music_folder $argv[1]
+    set music_folder $argv
   end
 
   mpv --profile=dontshowimage --gapless-audio --playlist (shuf (find $music_folder \
   -regextype posix-extended -regex ".*/*\.(mp3|wav|flac|m4a)\$" | psub) | psub)
+end
+
+function mpi --wraps=mpv --description="mpv image viewer"
+  mpv --profile=image $argv
 end
 
 #}}}
--- fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt
-    powerline-go -error $status -shell bare -newline -max-width 0
-end
