a6ae79ac617689748931c6e5551d639923ff9a98
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2018-05-24 00:42:21 +0200

    fish: colourized man pages

---
+++ fish/functions/man.fish
@@ -0,0 +1,12 @@
+# https://lobste.rs/s/eng7cz/adding_colors_man#c_1y27rr
+function man -d "Pretty manpages"
+  env \
+  LESS_TERMCAP_mb=(printf "\e[1;31m") \
+  LESS_TERMCAP_md=(printf "\e[1;31m") \
+  LESS_TERMCAP_me=(printf "\e[0m") \
+  LESS_TERMCAP_se=(printf "\e[0m") \
+  LESS_TERMCAP_so=(printf "\e[1;44;33m") \
+  LESS_TERMCAP_ue=(printf "\e[0m") \
+  LESS_TERMCAP_us=(printf "\e[1;32m") \
+  man $argv
+end
